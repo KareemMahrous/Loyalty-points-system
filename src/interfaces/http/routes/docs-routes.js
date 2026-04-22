2c46db831a6f76b8e421258409ca39081c5de06b
KareemMahrous <k.mahrous@ic4ti.com>
2026-04-22 16:26:14 +0200

    feat: implement swagger documentation routes for api specification access

--- src/interfaces/http/routes/docs-routes.js
+++ src/interfaces/http/routes/docs-routes.js
@@ -8,7 +8,17 @@ router.get("/json", (_req, res) => {
   res.status(200).json(swaggerSpec);
 });
 
+const options = {
+  customCssUrl: [
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css",
+  ],
+  customJs: [
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-standalone-preset.js",
+  ],
+};
+
 router.use("/", swaggerUi.serve);
-router.get("/", swaggerUi.setup(swaggerSpec));
+router.get("/", swaggerUi.setup(swaggerSpec, options));
 
 export { router as docsRouter };
